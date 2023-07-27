c44821ab6a639461a3e85a20418167009233f412
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2023-07-27 18:45:27 -0500

    Create basic content layout & design

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+
+
+const menuToggle = document.querySelector('.menuToggle');
+menuToggle.addEventListener('click', () => {
+    const mainMenu = document.querySelector('.mainMenu');
+    mainMenu.classList.toggle('hidden');
+});
